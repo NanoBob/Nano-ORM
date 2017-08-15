18d9e80695df978482b01ab9721909dd2d469b75
NanoBob <bhooff@avans.nl>
2017-08-15 16:40:39 +0200

    player join and quit event handling

--- server/resource/classes/Player.lua
+++ server/resource/classes/Player.lua
@@ -10,7 +10,7 @@ function Player:dataConstructor(success)
 	if success then
 		self:linkElement()
 	else
-		self.name = nil
+		outputServerLog("NO PLAYER MEN")
 		self:destroy()
 	end
 end
@@ -21,7 +21,7 @@ function Player:newConstructor(name)
 end
 
 function Player:destructor()
-	if self.name then
+	if self.element then
 		self.name = getPlayerName(self.element)
 		self.x, self.y, self.z = getElementPosition(self.element)
 		self.rx, self.ry, self.rz = getElementRotation(self.element)
@@ -32,4 +32,7 @@ end
 
 function Player:linkElement()
 	self.element = getPlayerFromName(self.name)
+	setCameraTarget(self.element,self.element)
+	fadeCamera(self.element,true)
+	spawnPlayer(self.element,self.x or 0,self.y or 0,self.z or 3,self.rz or 0,self.model or 7)
 end
--- server/resource/managers/PlayerManager.lua
+++ server/resource/managers/PlayerManager.lua
@@ -3,8 +3,10 @@ PlayerManager = inherit(Singleton,"PlayerManager")
 function PlayerManager:constructor()
 	self.players = {}
 	for _,playerElement in pairs(getElementsByType("player")) do
-		self:registerPlayer(playerElement)
+		self:addPlayer(playerElement)
 	end
+	addEventHandler("onPlayerJoin",getRootElement(),self.playerJoined.bind(self))
+	addEventHandler("onPlayerQuit",getRootElement(),self.playerLeft.bind(self))
 end
 
 function PlayerManager:destructor()
@@ -13,22 +15,34 @@ function PlayerManager:destructor()
 	end
 end
 
-function PlayerManager:registerPlayer(playerElement)
+function PlayerManager:playerJoined()
+	self:addPlayer(source)
+end
+
+function PlayerManager:playerLeft()
+	self:removePlayer(source)
+end
+
+function PlayerManager:addPlayer(playerElement)
 	local player = Player:select():where("name",getPlayerName(playerElement)):get(self.registerPlayerCallback.bind(self,playerElement))
 end
 
+function PlayerManager:removePlayer(playerElement)
+	local player = self.players[playerElement]
+	player:destroy()
+	self.players[playerElement] = nil
+end
+
 function PlayerManager:registerPlayerCallback(targetPlayer,players)
-	outputDebugString("PLAYER COUNT " .. #players)
 	if #players == 0 then
 		local player = Player:new(getPlayerName(targetPlayer))
 		player:save(self.newPlayerCallback.bind(self,player))
 	else
-		self.players[players[1].id] = players[1]	
+		self.players[targetPlayer] = players[1]	
 	end
 end
 
 function PlayerManager:newPlayerCallback(targetPlayer,id)
-	outputServerLog("NEW CALLBACK " .. id)
-	self.players[id] = targetPlayer
+	self.players[targetPlayer.element] = targetPlayer
 end
 
